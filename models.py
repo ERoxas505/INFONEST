0cec7662087d23fbc0b6199210061c792854c958
ERoxas505 <enzoroxas597@gmail.com>
2025-04-24 00:23:53 +0800

    final revision for now.

--- models.py
+++ models.py
@@ -1,57 +1,22 @@
 import streamlit as st
-from transformers.models.bart import BartForConditionalGeneration, BartTokenizer, BartConfig
+from transformers.models.bart import BartForConditionalGeneration, BartTokenizer
+from transformers import GenerationConfig 
 import os
 
 @st.cache_resource
 def load_bart_model():
-    token = os.getenv("HF_TOKEN")
-    
-    # Load the model configuration
-    config = BartConfig.from_pretrained(
-        "Angel0J/distilbart-multi_news-12-6_2",
-        use_auth_token=token
-    )
-    
-    # Set early_stopping in the config to a valid value
-    config.early_stopping = False  # Set to False to avoid the validation error
-    
-    # Initialize the model with the modified config
     model = BartForConditionalGeneration.from_pretrained(
-        "Angel0J/distilbart-multi_news-12-6_2",
-        config=config,  # Pass the modified config
-        use_auth_token=token
+        "sshleifer/distilbart-cnn-12-6",
+        use_safetensors=False
     ).to("cpu")
-    
     return model
 
 @st.cache_resource
 def load_bart_tokenizer():
-    token = os.getenv("HF_TOKEN")
     tokenizer = BartTokenizer.from_pretrained(
-        "Angel0J/distilbart-multi_news-12-6_2",
-        use_auth_token=token
+        "sshleifer/distilbart-cnn-12-6"
     )
     return tokenizer
-
-#import streamlit as st
-#from transformers.models.bart import BartForConditionalGeneration, BartTokenizer
-#from transformers import GenerationConfig 
-#import os
-
-#@st.cache_resource
-#def load_bart_model():
-    #model = BartForConditionalGeneration.from_pretrained(
-        #"sshleifer/distilbart-cnn-12-6",
-        #use_safetensors=False
-    #).to("cpu")
-    #return model
-
-#@st.cache_resource
-#def load_bart_tokenizer():
-    #tokenizer = BartTokenizer.from_pretrained(
-        #"sshleifer/distilbart-cnn-12-6"
-    #)
-    #return tokenizer
 
 #import streamlit as st
 #from transformers.models.bart import BartForConditionalGeneration, BartTokenizer
